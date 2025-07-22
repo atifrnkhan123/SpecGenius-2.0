69221549c3a4477848b3bc7ea51101302a917cdd
MOHAMMAD ATIF KHAN <atifrnkhan@gmail.com>
2025-07-22 20:10:46 +0000

    linkdin-https://www.linkedin.com/in/mohammad-atif-khan/ Developed by

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ export function Footer() {
                 SpecGenius | All data is processed locally on your browser.
             </div>
              <div className="text-center text-sm text-muted-foreground mt-1">
-                Developed by <a href="https://void.studio" target="_blank" rel="noopener noreferrer" className="text-primary/80 hover:underline hover:text-primary">Void Studio</a>
+                Developed by <a href="https://www.linkedin.com/in/mohammad-atif-khan/" target="_blank" rel="noopener noreferrer" className="text-primary/80 hover:underline hover:text-primary">Mohammad Atif Khan</a>
             </div>
         </footer>
     );
