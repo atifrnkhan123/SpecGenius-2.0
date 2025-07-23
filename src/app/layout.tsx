59580ada6bb1435f54b24a3fdb9a359d9d40accf
MOHAMMAD ATIF KHAN <atifrnkhan@gmail.com>
2025-07-23 15:35:32 +0000

    SpecGenius API Specification Analyzer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
-  title: 'SpecGenius',
+  title: 'SpecGenius API Specification Analyzer',
   description: 'SpecGenius is an API Specification Analyzer.',
 };
 
